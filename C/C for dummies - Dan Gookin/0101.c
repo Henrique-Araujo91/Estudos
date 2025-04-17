3a532450c21a14cdd05990e7a06c0ff50f165789
Henrique Araújo <henriquearaujo91@icloud.com>
2025-04-17 15:31:12 -0300

    Hello, world!

---
+++ C/C for dummies - Dan Gookin/0101.c
@@ -0,0 +1,12 @@
+/*As the saying goes, it brings bad luck
+not starting a new programming language study
+without a 'hello world' code*/
+
+#include <stdio.h>
+
+int main ()
+{
+    printf("Hello, world!\n");
+    printf("Olá, mundo!\n");
+    return 0;
+}
