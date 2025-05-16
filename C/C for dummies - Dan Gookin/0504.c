aafbfa2dbd3d412b3339b62671e4ed8b242f0e86
Henrique-Araujo91 <henriquearaujo91@icloud.com>
2025-05-16 16:00:26 -0300

    0504.c

---
+++ C/C for dummies - Dan Gookin/0504.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+puts("Valores 8 e 2:");
+printf("A soma é %d\n", 8 + 2);
+printf("A subtração é %d\n", 8 - 2);
+printf("A multiplicação é %d\n", 8 * 2);
+printf("A divisão é %d\n", 8 / 2);
+printf("O resto da divisão é %d\n", 8 % 2);
+return(0);    
+}
+
+// A side comment; i hated how vscode autocompleted this exercise.
+// Why are we even studying if the machine will do everything? 
+// This is all a bet. I hope it wil pay off.
+// Who knows.
