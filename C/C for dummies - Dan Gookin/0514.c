a677f52411f8923d7858255d3a4deb041b3e8196
Henrique-Araujo91 <henriquearaujo91@icloud.com>
2025-05-26 04:19:14 -0300

    0514.c

---
+++ C/C for dummies - Dan Gookin/0514.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define VALUE1 8
+#define VALUE2 2
+int main()
+{
+puts("Valores 8 e 2:");
+printf("A soma é %d\n", VALUE1 + VALUE2);
+printf("A subtração é %d\n", VALUE1 - VALUE2);
+printf("A multiplicação é %d\n", VALUE1 * VALUE2);
+printf("A divisão é %d\n", VALUE1 / VALUE2);
+printf("O resto da divisão é %d\n", VALUE1 % VALUE2);
+return(0);
+}
+
+// A side comment; i hated how vscode autocompleted this exercise.
+// Why are we even studying if the machine will do everything? 
+// This is all a bet. I hope it wil pay off.
+// Who knows.
+
+//...and it happened again.
