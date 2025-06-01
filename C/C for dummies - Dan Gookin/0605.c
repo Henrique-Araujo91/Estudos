8e00016f6819bf4d05dbc42b918f4109160541ec
Henrique-Araujo91 <henriquearaujo91@icloud.com>
2025-06-01 11:00:14 -0300

    0605.c

---
+++ C/C for dummies - Dan Gookin/0605.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int blorf;
+
+    blorf=22;
+
+    printf("O valor de blorf é %d.\n", blorf);
+    printf("O valor de blorf mais 16 é %d.\n", blorf + 16);
+    printf("O valor de blorf vezes ele próprio é %d.\n", blorf * blorf);
+    return(0);
+}
