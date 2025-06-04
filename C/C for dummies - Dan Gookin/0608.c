b289f35b79fc46a91f2d509095c022558c6606be
Henrique-Araujo91 <henriquearaujo91@icloud.com>
2025-06-04 01:32:43 -0300

    0608.c

---
+++ C/C for dummies - Dan Gookin/0608.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int shadrach, meshach, abednego;
+    shadrach= 701;
+    meshach= 709;
+    abednego=719;
+
+    printf("Shadrach é %d.\nMeshach é %d.\nAbednego é %d.\n", shadrach, meshach, abednego);
+    return(0);
+}
