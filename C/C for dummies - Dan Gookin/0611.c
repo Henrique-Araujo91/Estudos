ef20f3ea069e61b50144489ff6bc624080a4e2ef
Henrique-Araujo91 <henriquearaujo91@icloud.com>
2025-06-07 00:48:12 -0300

    0611.c

---
+++ C/C for dummies - Dan Gookin/0611.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int prime;
+
+    prime = 701;
+    printf("Shadrach é %d.\n", prime);
+    prime = 709;
+    printf("Meshach é %d.\n", prime);
+    prime = 719;
+    printf("Abednego é %d.\n", prime);
+    return(0);
+}
