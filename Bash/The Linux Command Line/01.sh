663e20b3df31f0fccf2bb14f43306076691958d6
Henrique-Araujo91 <henriquearaujo91@icloud.com>
2025-09-10 15:04:09 -0300

    01.sh

---
+++ Bash/The Linux Command Line/01.sh
@@ -0,0 +1,22 @@
+#What is the shell?
+
+#Assuming you arent using an graphical user interface, you can interact with your computer using a terminal, which is a text-based interface that allows you to execute commands and perform tasks. The shell is the program that interprets these commands and provides a way for you to interact with the operating system. Assuming you are using linux, Circling through ctrl alt f1 to ctrl alt f6 will take you to a virtual terminal, which is a text-based interface that allows you to interact with the shell directly. You can switch back to your graphical user interface by pressing ctrl alt f7 or ctrl alt f8, depending on your system.
+#Terminal windows, by other hand, are programs that runs over the graphical user interface and allows you to interact with the shell. You can open a terminal window by searching for "Terminal" in your applications menu or by using a keyboard shortcut (usually Ctrl+Alt+T).
+
+#Basic Commands
+
+#date - Displays the current date and time
+date; 
+
+#cal - Displays the current month's calendar
+sudo apt install ncal;
+cal;
+
+#df - Displays the disk space usage of file systems
+df;
+
+#free - Displays the amount of free and used memory in the system
+free;
+
+#the 'exit' command is used to exit the shell window, which we dont want to do now. Besides exit, you can use Ctrl+D to exit the shell.
+#exit;
