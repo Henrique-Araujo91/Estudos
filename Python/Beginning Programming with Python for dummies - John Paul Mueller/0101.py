872e4502be4d1167378295d9a73eb5092e06b955
Henrique-Araujo91 <henriquearaujo91@icloud.com>
2025-09-16 00:03:36 -0300

    0101.py

---
+++ Python/Beginning Programming with Python for dummies - John Paul Mueller/0101.py
@@ -0,0 +1,13 @@
+#As the saying goes, it brings bad luck
+#not starting a new programming language study
+#without a 'hello world' code
+
+#For some reason, mr Mueller
+#asked for a different message, 
+#but i prefer to stick with 
+#the original one, at least for the
+#first code
+
+print("Hello, world!")
+
+print("Olá, mundo!")
