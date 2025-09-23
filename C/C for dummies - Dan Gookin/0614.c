a5abe019a57fd3c90e16f4da8548160d391b0d45
Henrique-Araujo91 <henriquearaujo91@icloud.com>
2025-09-23 07:33:20 -0300

    0614.c

---
+++ C/C for dummies - Dan Gookin/0614.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("O valor é %d\n",3);
+    printf("E %d é o valor.\n",3);
+    printf("Não é %d\n",3+1);
+    printf("E não é %d\n",3-1);
+    printf("Não, o valor é %d\n",3);
+    return(0);
+}
