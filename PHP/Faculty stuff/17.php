0db7d092a819eff23d8eeb0a1128783a65c4ba25
Henrique-Araujo91 <henriquearaujo91@icloud.com>
2025-10-18 01:39:03 -0300

    17.php

---
+++ PHP/Faculty stuff/17.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+        //Criação de um array indexado
+        $frutas = array("Banana","Uva","Laranja");
+        ?>
+
+        <?php //Acessando elemento 0 do array
+        echo $frutas[0];
+        ?>
+        <br>
+
+        <?php //Acessando elemento 1 do array
+        echo $frutas[1];
+        ?>
+        <br>
+
+        <?php //Acessando elemento 2 do array
+        echo $frutas[2];
+        ?>
+    </body>
+</html>
