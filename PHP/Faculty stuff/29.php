632fe4dace36163bcbde58a71366b9c55fdc97a2
Henrique-Araujo91 <henriquearaujo91@icloud.com>
2025-10-31 00:36:58 -0300

    29.php

---
+++ PHP/Faculty stuff/29.php
@@ -0,0 +1,10 @@
+<?php
+//Conexão persistente com o postgreSQL
+
+try {
+
+	$dsn = new PDO("pgsql:host=". HOST . ";port=".PORT.";dbname=" . DBNAME .";user=" . USER . ";password=" . PASSWORD, array(PDO::ATTR_PERSISTENT => true)) ;
+
+} catch (PDOException $e) {
+	echo 'A conexão falhou e retornou a seguinte mensagem de erro: ' .$e->getMessage();
+}
