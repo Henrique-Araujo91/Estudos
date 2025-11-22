3a1fb9f56195a32c82361a50e8be3db29ff3824b
Henrique-Araujo91 <henriquearaujo91@icloud.com>
2025-11-22 11:40:14 -0300

    0706.c

---
+++ C/C for dummies - Dan Gookin/0706.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int ch;
+    printf("Pressione Enter: ");
+    getchar();
+    ch = 'O';
+    putchar(ch);
+    ch = 'i';
+    putchar(ch);
+    putchar('!');
+    putchar('\n');
+    return(0);
+}
