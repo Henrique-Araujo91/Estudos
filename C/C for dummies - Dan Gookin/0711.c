61f8a7420eafe84e9abc3082f0ac97420b822e12
Henrique-Araujo91 <henriquearaujo91@icloud.com>
2025-11-27 13:29:11 -0300

    0711.c

---
+++ C/C for dummies - Dan Gookin/0711.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    char prompt[]  = "Programa para Destruir o mundo\nPressione Enter para explodir: ";
+
+    printf("%s",prompt);
+    getchar();
+    return(0);
+}
