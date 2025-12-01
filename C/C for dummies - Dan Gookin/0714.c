fee8a840a64d38a43e362a7088d1f5bf31c813ff
Henrique-Araujo91 <henriquearaujo91@icloud.com>
2025-11-30 22:32:17 -0300

    0714.c

---
+++ C/C for dummies - Dan Gookin/0714.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int fav;
+
+    printf("Qual o seu número favorito?\n");
+    scanf("%d",&fav);
+    printf("%d é meu número favorito também!\n",fav);
+    return(0);
+}
