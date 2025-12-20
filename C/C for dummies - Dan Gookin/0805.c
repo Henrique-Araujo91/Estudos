536c42537e0a24502485b9e132ce374cf6bc64a9
Henrique-Araujo91 <henriquearaujo91@icloud.com>
2025-12-19 23:52:58 -0300

    0805.c

---
+++ C/C for dummies - Dan Gookin/0805.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int first,second;
+
+    printf("Entre com o primeiro valor: ");
+    scanf("%d",&first);
+    printf("Entre com o segundo valor: ");
+    scanf("%d",&second);
+
+    puts("Avaliando...");
+    if(first<second)
+    {
+        printf("%d é menor que %d\n",first,second);
+    }
+    if(first>second)
+    {
+        printf("%d é maior que %d\n",first,second);
+    }
+    if(first==second)
+    {
+        printf("%d é igual a %d\n",first,second);
+    }
+    return(0);
+}
